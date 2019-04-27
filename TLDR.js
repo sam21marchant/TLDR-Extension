d953c62fe08d86e1927b54ff43d9b97601e77132
Himani Vishal Dodeja <himanivi@buffalo.edu>
2019-04-27 12:00:15 -0400

    event listener in js

---
+++ TLDR.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var startButton = document.getElementById('startTLDR');
+    //fetch data
+    startButton.addEventListener('click',function() { //starts TLDR progess, write algorithm here
+
+        //var myHeading = document.querySelector('h1');
+        //myHeading.textContent = 'Hello world';
+    })
+});
