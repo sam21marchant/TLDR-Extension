58f69052b664b2c83bf3cf3c21b59e4e3da48f6d
Himani Vishal Dodeja <himanivi@buffalo.edu>
2019-04-27 14:59:23 -0400

    for liam

--- TLDR.js
+++ TLDR.js
@@ -22,11 +22,16 @@ function getSiteData(url) {
 }
 
 function tldr(html) {
-    var htmlData = ["the", "big", "dog", "dog"]; //break up incoming data, split by space to get list of words
-    var freqMap = new Map([]);
+    var wordData = ["big", "dog", "it","is","so","big"]; //break up incoming data, split by space to get list of words
+    var dataInSentences = ["big dog.", "it is so uuge.", "people disagree it's big."];
+    var freqMap = new Map([]); //stores words with the # of times they're said
+    var badWords = ["in","a","an","the"];
 
-    for(var i = 0; i < htmlData.length; i++) {
+    for(var i = 0; i < htmlData.length; i++) { //fills map with words and the frequency of them
         var key = htmlData[i];
+        if(badWords.includes(key)) {
+
+        }
         if(freqMap.has(key)) {
             var newVal = freqMap.get(key) + 1;
             freqMap.set(key,newVal);
@@ -34,6 +39,13 @@ function tldr(html) {
         else {
             freqMap.set(key, 1);
         }
+    }
+
+    //check words in sentence, find point total
+    //sentence with highest point total goes in TLDR
+    for(var i = 0; i < dataInSentences.length; i++) {
+        var sentencePointTotal = 0;
+
     }
     Console.log(freqMap);
 }
