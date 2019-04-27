2b125fe6461e980844c6ec956727c64ce97d8162
Himani Vishal Dodeja <himanivi@buffalo.edu>
2019-04-27 12:36:47 -0400

    basic algorithm code

--- TLDR.js
+++ TLDR.js
@@ -7,3 +7,20 @@ document.addEventListener('DOMContentLoaded', function() {
         //myHeading.textContent = 'Hello world';
     })
 });
+
+function tldr(html) {
+    var htmlData = ["the", "big", "dog", "dog"]; //break up incoming data, split by space to get list of words
+    var freqMap = new Map([]);
+
+    for(var i = 0; i < htmlData.length; i++) {
+        var key = htmlData[i];
+        if(freqMap.has(key)) {
+            var newVal = freqMap.get(key) + 1;
+            freqMap.set(key,newVal);
+        }
+        else {
+            freqMap.set(key, 1);
+        }
+    }
+    Console.log(freqMap);
+}
